b5a9bf8f9dfb133136a2c2e8c102e5bd6fcdc933
vietductran16092003 <vietductran16092003@gmail.com>
2026-08-24 14:26:08 +0700

    feat(public): wrap public layout in SessionProvider

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,13 +1,16 @@
+import { SessionProvider } from "next-auth/react";
 import { PublicLangProvider } from "@/lib/public-i18n";
 import { PublicHeader } from "@/components/public-header";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <PublicLangProvider>
-      <div className="flex min-h-screen flex-col">
-        <PublicHeader />
-        <main className="flex-1">{children}</main>
-      </div>
-    </PublicLangProvider>
+    <SessionProvider>
+      <PublicLangProvider>
+        <div className="flex min-h-screen flex-col">
+          <PublicHeader />
+          <main className="flex-1">{children}</main>
+        </div>
+      </PublicLangProvider>
+    </SessionProvider>
   );
 }
